6deff6891dd8645d4fef654f72e83608ff5324fc
Ekaterina Buyakova <ekaterinabuyakova602@gmail.com>
2020-08-18 23:10:43 +0300

    regions migration and admin guard

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        //
     ];
 
     /**
@@ -25,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('admin-panel', function (User $user) {
+            return $user->isAdmin();
+        });
     }
 }
